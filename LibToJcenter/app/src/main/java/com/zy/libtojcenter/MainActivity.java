96f6ed565c983ddf5e2b54f697c4efce325c80dc
zy <584450903@qq.com>
2018-12-18 15:23:23 +0800

    Signed-off-by: zy <584450903@qq.com>

---
+++ LibToJcenter/app/src/main/java/com/zy/libtojcenter/MainActivity.java
@@ -0,0 +1,17 @@
+package com.zy.libtojcenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zy.testlib.Test;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Test.toast(this);
+    }
+}
---
+++ LibToJcenter/testlib/src/main/java/com/zy/testlib/Test.java
@@ -0,0 +1,10 @@
+package com.zy.testlib;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Test {
+    public static void toast(Context context){
+        Toast.makeText(context,"YY",Toast.LENGTH_LONG).show();
+    }
+}
